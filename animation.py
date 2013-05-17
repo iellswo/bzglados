43644040a07189faf2b4f623e0650ae4517ae8d2
Joshua David Lepinski <ljoshuad@nunchucks.cs.byu.edu>
2013-05-16 20:11:07 -0600

    it takes foreverls

--- animation.py
+++ animation.py
@@ -12,103 +12,12 @@ import math
 class ANIMATION:
     
     
-    def __init__(self, WS, SP, OB, OS, GL, GR, GS):
+    def __init__(self, WS, OB):
         """Initialize variables"""
-        self.spread=SP
         self.worldSize=WS
-        self.obsScale=OS
-        self.goal={'radius' : float(GR), 'center' : GL, 'type' : "pull", 'scale' : float(GS)}
+        
         self.obstacles=OB
-        self.fieldGenerators = []
-        for ob in OB:
-            self.obsRadiusAndCenter(ob)
-            
-
-    def update(self, GL, GR, GS):
-        self.goal={'radius' : float(GR),'center' : GL, 'type' : "pull", 'scale' : float(GS)}
-        
    
-    ########################################################################
-    # Field and Obstacle Definitions
-
-
-        #this determines our arrow at the point x y I've added the attractive field case
-     
-       
-    def generate_repulsive_fields(self, x, y):
-        
-        fx = 0.0
-        fy = 0.0
-        
-        for fg in self.fieldGenerators:
-            if fg['type'] == 'push':
-                #print fg
-                distance = math.sqrt( (fg['center'][0] - x)**2 +  (fg['center'][1] - y)**2 )
-                theta=math.atan2((fg['center'][1] - y), (fg['center'][0] - x))
-            
-                if distance<fg['radius']:
-                    fx += -math.copysign(10, math.cos(theta))
-                    fy += -math.copysign(10, math.sin(theta))
-                elif fg['radius'] <= distance <= (self.spread + fg['radius']):
-                    fx += -fg['scale'] * (self.spread + fg['radius'] - distance) * math.cos(theta)
-                    fy += -fg['scale'] * (self.spread + fg['radius'] - distance) * math.sin(theta)
-                elif distance>(self.spread+fg['radius']):
-                    fx += 0.0
-                    fy += 0.0
-                
-        return fx, fy
-    
-    def generate_attractive_fields(self, x, y, fg):
-        
-        fx = 0.0
-        fy = 0.0
-        # return the vector for this location
-        distance = math.sqrt( (fg['center'][0] - x)**2  +  (fg['center'][1] - y)**2 )
-        theta = math.atan2((fg['center'][1] - y), (fg['center'][0] - x))
-        if distance < fg['radius']:
-            fx = 0
-            fx = 0
-        elif fg['radius'] <= distance <= (self.spread + fg['radius']):
-            fx = fg['scale'] * (distance-fg['radius']) * math.cos(theta)
-            fy = fg['scale'] * (distance-fg['radius']) * math.sin(theta)
-        elif distance>(self.spread+fg['radius']):
-            fx = fg['scale'] * math.cos(theta)
-            fy = fg['scale'] * math.sin(theta)
-        
-        return fx, fy
-        
-    def generate_tangental_fields(self,x, y):
-        
-        fx = 0.0
-        fy = 0.0
-        
-        for fg in self.fieldGenerators:
-            if fg['type'] == 'push':
-                #print fg
-                distance = math.sqrt( (fg['center'][0] - x)**2 +  (fg['center'][1] - y)**2 )
-                theta = math.atan2((fg['center'][1] - y), (fg['center'][0] - x)) + math.pi/4
-            
-                if distance<fg['radius']:
-                    fx += -math.copysign(50, math.cos(theta))
-                    fy += -math.copysign(50, math.sin(theta))
-                elif fg['radius'] <= distance <= (self.spread + fg['radius']):
-                    fx += -fg['scale']*2 * (self.spread + fg['radius'] - distance) * math.cos(theta)
-                    fy += -fg['scale']*2 * (self.spread + fg['radius'] - distance) * math.sin(theta)
-                elif distance>(self.spread+fg['radius']):
-                    fx += 0.0
-                    fy += 0.0
-                
-        return fx, fy
-
-    def generate_fields(self, x, y):
-        
-        x1, y1 = self.generate_attractive_fields(x, y, self.goal)
-        #print "attractive", x1, y1
-        x2, y2 = self.generate_repulsive_fields(x, y)
-        #print "repulsive", x2, y2
-        x3, y3 = self.generate_tangental_fields(x, y)
-        #print "tangental", x3, y3
-        return (x+x1+x2+x3), (y+y1+y2+y3)
         
     def animate(self,path,orderedVisits):
         try:
@@ -118,35 +27,35 @@ class ANIMATION:
             import sys
             sys.exit(-1)
         gp = GnuplotProcess(persist=True)
-        gp.write(gnuplot_header(-self.worldsize / 2, self.worldsize / 2))
-        gp.write(draw_obstacles(self.obstacles)) 
-        gp.write(draw_nodes(orderedVisits))
-        gp.write(draw_path(path))
+        gp.write(self.gnuplot_header(-self.worldSize / 2, self.worldSize / 2))
+        gp.write(self.draw_obstacles(self.obstacles)) 
+        gp.write(self.draw_nodes(orderedVisits))
+        gp.write(self.draw_path(path))
         
     def draw_path(self,path):
         '''Return a string which tells Gnuplot to draw all of the obstacles.'''
-        s = 'unset arrow\n'
+        #s = 'unset arrow\n'
+        s = ''
 
-        for p in path:
-            red=1
-            last_point = p[0]
-            for cur_point in p[1:]:
-                s += draw_line(last_point, cur_point, red)
-                last_point = cur_point
+        red=1
+        last_point = path[0]
+        for cur_point in path[1:]:
+            s += self.draw_line(last_point, cur_point, red)
+            last_point = cur_point
         return s
 
     def draw_nodes(self,nodes):
         '''Return a string which tells Gnuplot to draw all of the obstacles.'''
-        s = 'unset arrow\n'
+        #s = 'unset arrow\n'
+        s = ''
 
-        for n in nodes:
-            green=2
-            last_point = n[0]
-            for cur_point in n[1:]:
-                s += draw_line(last_point, cur_point, green)
-                last_point = cur_point
-            
-            s+="\nplot '-' with lines\n0 0 0 0\ne\npause %f\n" % (1.0)
+        green=2
+        last_point = nodes[0]
+        for cur_point in nodes[1:]:
+            s += self.draw_line_pause(last_point, cur_point, green)
+            last_point = cur_point
+        
+        
            
         return s
         
@@ -167,14 +76,14 @@ class ANIMATION:
     def draw_obstacles(self,obstacles):
         '''Return a string which tells Gnuplot to draw all of the obstacles.'''
         s = 'unset arrow\n'
-        brown=6
+        brown=3
         for obs in obstacles:
             last_point = obs[0]
             for cur_point in obs[1:]:
-                s += draw_line(last_point, cur_point, brown)
+                s += self.draw_line(last_point, cur_point, brown)
                 last_point = cur_point
-            s += draw_line(last_point, obs[0], brown)
-           
+            s += self.draw_line(last_point, obs[0], brown)
+        
         return s
                   
     def draw_line(self,p1, p2,color):
@@ -185,37 +94,21 @@ class ANIMATION:
         x2 = p2[0]
         y2 = p2[1]
         s='set arrow from %s, %s to %s, %s nohead lt %d\n' % (x1, y1, x2, y2 ,color)
-       
-        return s
-   
-
-    ########################################################################
-    # Helper Functions
-
-   
-
-    def obsRadiusAndCenter(self, tup):
-        xmin=float("inf")
-        xmax=float("-inf")
-        ymin=float("inf")
-        ymax=float("-inf")
-        for pairs in tup:
-            if float(pairs[0])>xmax:
-                xmax=float(pairs[0])
-            if float(pairs[0])<xmin:
-                xmin=float(pairs[0])
-            if float(pairs[1])>ymax:
-                ymax=float(pairs[1])
-            if float(pairs[1])<ymin:
-                ymin=float(pairs[1])
-        temp = ( ( math.sqrt( float(xmax-xmin)**2 + float(ymax-ymin)**2)/2 ), 
-        (float(xmax-xmin)/2 + xmin, float(ymax-ymin)/2 + ymin), 
-        "push")
-        fg = {'radius' : temp[0], 'center' : temp[1], 'type' : temp[2], 'scale' : .5}
-        self.fieldGenerators.append(fg)
         
-        return temp
-          
+        return s
+        
+    def draw_line_pause(self, p1, p2, color):
+        '''Return a string to tell Gnuplot to draw a line from point p1 to
+        point p2 in the form of a set command.'''
+        x1 = p1[0]
+        y1 = p1[1]
+        x2 = p2[0]
+        y2 = p2[1]
+        s='set arrow from %s, %s to %s, %s nohead lt %d\n' % (x1, y1, x2, y2 ,color)
+        s+="\nplot '-' with lines\n0 0 0 0\ne\npause %f\n" % (0.0001)
+        return s
+       
+   
 
 
 
--- searchagent.py
+++ searchagent.py
@@ -3,10 +3,16 @@
 import sys
 import math
 
-from bzrc import BZRC, Command
+from animation import ANIMATION as Animation
 
+from collections import deque
+
+from bzrc import BZRC, Command
+    
 class State(object):
-    """A state keeps track of things used by the various searches."""
+    pass
+
+class Problem(object):
     pass
 
 class Agent(object):
@@ -15,6 +21,7 @@ class Agent(object):
         self.bzrc = bzrc
         self.constants = self.bzrc.get_constants()
         self.mytank = self.bzrc.get_mytanks()[0]
+        #print self.mytank.x, self.mytank.y
         start, grid = self.bzrc.get_occgrid(0)
         self.grid = []
         self.offset = int(self.constants['worldsize'])/2
@@ -30,12 +37,17 @@ class Agent(object):
             #print self.grid[int(tank.x)+self.offset][int(tank.y)+self.offset]
         self.goal = [flag for flag in self.bzrc.get_flags() if flag.color == 'green'][0]
         #print self.goal.color, self.goal.x, self.goal.y 
+        self.ani = Animation(int(self.constants['worldsize']), self.bzrc.get_obstacles())
         
     def search(self, algorithm):
         if algorithm == 'depthfirst' or algorithm == 'dfs':
-            pass
+            return self.depthFirstSearch()
         elif algorithm == 'breadthfirst' or algorithm == 'bfs':
-            pass
+            problem = Problem()
+            problem.starting = (int(self.mytank.x + self.offset), int(self.mytank.y + self.offset))
+            problem.goal = (int(self.goal.x + self.offset), int(self.goal.y + self.offset))
+            #problem.goal = (int(self.mytank.x + self.offset + 10), int(self.mytank.y + self.offset))
+            self.breadthFirstSearch(problem)
         elif algorithm == 'idastar':
             pass
         elif algorithm == 'uniformcost' or algorithm == 'ucs':
@@ -43,14 +55,81 @@ class Agent(object):
         elif algorithm == 'astar':
             pass
         else:
-            raise Exception(algorithm)
+            raise Exception('Unknown algorithm %d' % algorithm)
     
     def depthFirstSearch(self):
         """Returns a list of tuples, being x, y coordinates on the grid
            being the path to the goal found via a depth first search"""
+     
+    def breadthFirstSearch(self, problem):
+        """Returns a list of tuples, being x, y coordinates on the grid
+           being the path to the goal found via a depth first search"""
+        frontier=deque([])
+        if problem.starting == problem.goal:
+            return [problem.starting]
+        node = State()
+        node.coord = problem.starting
+        node.parent = None
+        frontier.append( node )
+        explored=[]
+        while True:
+            if len(frontier) == 0:
+                raise Exception('failure')
+            node = frontier.popleft()
+            explored.append(node)
+            for n in self.get_neighbors(node.coord):
+                fc = [f.coord for f in frontier]
+                ec = [e.coord for e in explored]
+                if n not in fc and n not in ec:
+                    if self.grid[n[0]][n[1]][2] == 1:
+                        continue
+                    #print n
+                    newnode = State()
+                    newnode.coord = n
+                    newnode.parent = node
+                    if n == problem.goal:
+                        s = self.make_solution(newnode)
+                        self.ani.animate(s, [self.adjust(e.coord) for e in explored])
+                        return s
+                    frontier.append(newnode)
+    
+    def get_neighbors(self, node):
+        l = []
+        if not self.is_outside_bounds( (node[0]-1, node[1]) ):
+            l.append( (node[0]-1, node[1]) )
+        if not self.is_outside_bounds( (node[0]+1, node[1]) ):
+            l.append( (node[0]+1, node[1]) )
+        if not self.is_outside_bounds( (node[0], node[1]+1) ):
+            l.append( (node[0], node[1]+1) )
+        if not self.is_outside_bounds( (node[0], node[1]-1) ):
+            l.append( (node[0], node[1]-1) )
+        if not self.is_outside_bounds( (node[0]-1, node[1]+1) ):
+            l.append( (node[0]-1, node[1]+1) )
+        if not self.is_outside_bounds( (node[0]+1, node[1]+1) ):
+            l.append( (node[0]+1, node[1]+1) )
+        if not self.is_outside_bounds( (node[0]-1, node[1]-1) ):
+            l.append( (node[0]-1, node[1]-1) )
+        if not self.is_outside_bounds( (node[0]+1, node[1]-1) ):
+            l.append( (node[0]+1, node[1]-1) )
+        return l
         
+    def is_outside_bounds(self, node):
+        return (node[0] < 0 or node[1] < 0 or
+                node[0] > int(self.constants['worldsize']) - 1 or
+                node[1] > int(self.constants['worldsize']) - 1)
+                
+    def make_solution(self, node):
+        a = []
+        while node.parent != None:
+            a.append( self.adjust(node.coord) )
+            node = node.parent
+        a.append( self.adjust(node.coord) )
+        a.reverse()
+        return a
         
-        
+    def adjust(self, coord):
+        return (self.grid[coord[0]][coord[1]][0], self.grid[coord[0]][coord[1]][1])
+    
 def main():
     # Process CLI arguments.
     try:
@@ -66,11 +145,8 @@ def main():
     
     agent = Agent(bzrc)
     
-    try:
-        agent.search(algorithm)
-    except Exception as e:
-        print 'Error: unrecognized algorithm: %s' % str(e)
-        print 'valid options: dfs, bfs, idastar, ucs, astar'
+    agent.search(algorithm)
+
 
 if __name__ == '__main__':
     main()
