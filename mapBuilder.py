e2775265a171173e45d67ab151526c66eaaa9538
Joshua David Lepinski <ljoshuad@pike.cs.byu.edu>
2013-05-30 19:51:54 -0600

    created map builder

---
+++ mapBuilder.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import OpenGL
+import time
+OpenGL.ERROR_CHECKING = False
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from numpy import zeros
+class MapBuilder:
+    
+    def __init__(self,h,w):
+        self.init_window(h,w)
+        
+    def draw_grid(self):
+        # This assumes you are using a numpy array for your grid
+        self.width, self.height = self.grid.shape
+        glRasterPos2f(-1, -1)
+        glDrawPixels(self.width, self.height, GL_LUMINANCE, GL_FLOAT, self.grid)
+        glFlush()
+        glutSwapBuffers()
+
+    def update_grid(self,new_grid):
+        self.grid = new_grid
+
+
+    def init_window(self, width, height):
+        global window
+        self.width=width
+        self.height=height
+        self.grid = zeros((self.width, self.height))
+        glutInit(())
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+        glutInitWindowSize(self.width, self.height)
+        glutInitWindowPosition(20, 20)
+        window = glutCreateWindow("Grid filter")
+        glutDisplayFunc(self.draw_grid)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        
+        
+        
+      
+
+    def hang(self):
+        glutMainLoop()
+
+
+
+
+
+
+
+
+
+# vim: et sw=4 sts=4
