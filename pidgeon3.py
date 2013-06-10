2174f472d7a1b026533e8d3a0df8a9114e03d451
Ike Ellsworth <isaac@kina.net>
2013-06-10 16:13:22 -0600

    our pidgeon wasn't wild enough

--- pidgeon3.py
+++ pidgeon3.py
@@ -1,67 +1,65 @@
 import sys
 import math
-import time
 import random
+import time
 
 from bzrc import BZRC, Command
 
+def ahead(tank):
+    print 'ahead'
+    command = Command(tank.index, .8, 0, False)
+    return command
+    
+def backup(tank):
+    print 'backup'
+    command = Command(tank.index, -.8, 0, False)
+    return command
+    
+def sharp_turn(tank):
+    print 'sharp turn'
+    command = Command(tank.index, .9, random.choice([-1, 1]), False)
+    return command
+    
+def wide_turn(tank):
+    print 'wide turn'
+    an = tank.angvel if tank.angvel**2 > .05 else random.choice([-.5, .5])
+    command = Command(tank.index, .7, an, False)
+    return command
+    
+def stop(tank):
+    return Command(tank.index, 0, 0, True)
+    
+STATES = [ahead, backup, sharp_turn, wide_turn]
+    
+times = [2.0, 2.7, 2.5, 2.3]
 
 class Agent(object):
-    """Class handles all command and control logic for a teams tanks."""
-
+    
     def __init__(self, bzrc):
+        # bzrc connections
         self.bzrc = bzrc
         self.constants = self.bzrc.get_constants()
         self.commands = []
-        mytanks=bzrc.get_mytanks()
-        self.target_by_tank_num=[]
-        for tank in mytanks:
-            self.target_by_tank_num.append((int(self.constants['worldsize'])/2)-50)
-       
-
+        
+        self.step = 0
+        self.state = 0
+        
     def tick(self, time_diff):
-        """Some time has passed; decide what to do next."""
         mytanks, othertanks, flags, shots = self.bzrc.get_lots_o_stuff()
-        self.mytanks = mytanks
-        self.othertanks = othertanks
-        self.flags = flags
-        self.shots = shots
-        self.enemies = [tank for tank in othertanks if tank.color !=
-                        self.constants['team']]
-
-        self.commands = []
-
-        for tank in mytanks:
-            if tank.y-(self.target_by_tank_num[tank.index])==0:
-                self.target_by_tank_num[tank.index]=-1*self.target_by_tank_num[tank.index]
-                #print "switching target"
-                
-            self.move_to_position(tank,(0,self.target_by_tank_num[tank.index]))
-
-        results = self.bzrc.do_commands(self.commands)
-
+        self.step += time_diff
+        if self.step > times[self.state]:
+            #self.commands.append(stop(mytanks[0]))
+            self.step = 0.0
+            self.state = random.randint(0, 3)
+            for tank in mytanks:
+                self.commands.append(STATES[self.state](tank))
+                self.bzrc.do_commands(self.commands)
+        
+        
+            
+        
+        
     
-
-    def move_to_position(self, tank, target):
-        """Set command to move to given coordinates."""
-        target_x, target_y = target
-        target_angle = math.atan2(target_y - tank.y,
-                                  target_x - tank.x)
-        relative_angle = self.normalize_angle(target_angle - tank.angle)
-        wiggle = random.uniform(-.1, .1)
-        command = Command(tank.index, max(-.03, random.uniform(.3, 1)-abs(relative_angle)), relative_angle+wiggle, False)
-        self.commands.append(command)
-
-    def normalize_angle(self, angle):
-        """Make any angle be between +/- pi."""
-        angle -= 2 * math.pi * int (angle / (2 * math.pi))
-        if angle <= -math.pi:
-            angle += 2 * math.pi
-        elif angle > math.pi:
-            angle -= 2 * math.pi
-        return angle
-
-
 def main():
     # Process CLI arguments.
     try:
@@ -83,7 +81,9 @@ def main():
     # Run the agent
     try:
         while True:
-            time_diff = time.time() - prev_time
+            now = time.time()
+            time_diff = now - prev_time
+            prev_time = now
             agent.tick(time_diff)
     except KeyboardInterrupt:
         print "Exiting due to keyboard interrupt."
